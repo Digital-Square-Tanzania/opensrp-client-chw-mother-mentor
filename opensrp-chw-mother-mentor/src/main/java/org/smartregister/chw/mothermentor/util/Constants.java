4cf0f74fd93720fcdbd21dfa1ff30a38961e4263
cozej4 <ilakozejumanne@gmail.com>
2026-05-28 08:42:32 +0300

    🧹 Remove unused procedure constants

--- opensrp-chw-mother-mentor/src/main/java/org/smartregister/chw/mothermentor/util/Constants.java
+++ opensrp-chw-mother-mentor/src/main/java/org/smartregister/chw/mothermentor/util/Constants.java
@@ -86,35 +86,4 @@ public interface Constants {
         String MOTHERMENTOR_PROFILE = "mothermentor_profile";
     }
 
-    interface VALUES {
-        String NONE = "none";
-        String CHORDAE = "chordae";
-        String HIV = "hiv";
-        String RBG = "random_blood_glucose_test";
-        String FBG = "fast_blood_glucose_test";
-        String HYPERTENSION = "hypertension";
-        String SILICON_OR_LEXAN = "silicon_or_lexan";
-        String NEGATIVE = "negative";
-        String SATISFACTORY = "satisfactory";
-        String NEEDS_FOLLOWUP = "needs_followup";
-        String YES = "yes";
-    }
-
-    interface TABLE_COLUMN {
-        String GENITAL_EXAMINATION = "genital_examination";
-        String SYSTOLIC = "systolic";
-        String DIASTOLIC = "diastolic";
-        String ANY_COMPLAINTS = "any_complaints";
-        String CLIENT_DIAGNONISED_WITH = "is_client_diagnosed_with_any";
-        String COMPLICATION_TYPE = "type_complication";
-        String HEMATOLOGICAL_DISEASE_SYMPTOMS = "any_hematological_disease_symptoms";
-        String KNOWN_ALLEGIES = "known_allergies";
-        String HIV_RESULTS = "hiv_result";
-        String HIV_VIRAL_LOAD = "hiv_viral_load_text";
-        String TYPE_OF_BLOOD_FOR_GLUCOSE_TEST = "type_of_blood_for_glucose_test";
-        String BLOOD_FOR_GLUCOSE = "blood_for_glucose";
-        String DISCHARGE_CONDITION = "discharge_condition";
-        String IS_MALE_PROCEDURE_CIRCUMCISION_CONDUCTED = "is_male_procedure_circumcision_conducted";
-    }
-
 }
